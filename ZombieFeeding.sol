4c3c69477f2403f377c32ef244549173e286a818
Umar Quadri <umar.quad@gmail.com>
2018-03-20 15:02:18 -0700

    Added KittyInterface to interact with crypto kitties and feedonKitty function

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -24,18 +24,25 @@ contract ZombieFeeding {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint _zombieId,uint _targetDna) public{
-      //make sure user can access only his zombie
+    function feedAndMultiply(uint _zombieId, uint _targetDna,string _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
-
-        //make new Dna from host and zombie and use that to create new zombie
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
+        //modify last two digits of dna to identify that the zombie fed on kitty
+        if(keccak256(_species)==keccak256("kitty")){
+          newDna = newDna - newDna % 100 + 99;
+        }
         _createZombie("NoName", newDna);
+    }
 
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        //get the kittyDna from the crypto Kitties smart contract
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
 
-  }
+        feedAndMultiply(_zombieId, kittyDna,"kitty");
+    }
 
 
 }
